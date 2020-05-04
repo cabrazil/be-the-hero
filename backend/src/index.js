4b7175afce7b065a4c8847bdc69f710273e78a47
Carlos Brazil <br2go@vps.server.local>
2020-05-03 22:52:34 -0300

    Finalizado backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const cors = require('cors');   
+const cors = require('cors');
+const dotenv = require('dotenv');    
 const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
 
-app.use(cors);
+//app.use(cors);
 // se colocar em produção o projeto, deveria usar opção origin: com endereço que pode acessar a aplicação
 // se tivesse hospedado em 'http://meuapp.com'
-app.use(express.json());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+	  //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use(routes);
 
-app.listen(3333);
+dotenv.config();
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
