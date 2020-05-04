6679ea43781d167a9d8e6859fde82322e97c6312
Carlos Brazil <br2go@vps.server.local>
2020-05-03 22:52:34 -0300

    Alterado backend para acessar BD Postgresql

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -5,17 +5,13 @@ module.exports = {
         const { page = 1 } = request.query;
 
         const [count] = await connection('incidents').count();
-// variável count está entre [] pois pode retornar um array e quero pegar somente o primeiro ou count[0]
         
-        response.header('X-Total-Count', count['count(*)']);
-// no cabeçalho da resposta da requisição colocar o total de registros
         const incidents = await connection('incidents')
-        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+          .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
 // join para trazer algumas colunas da tabela ongs
-        .limit(5)
-        .offset((page -1)* 5)
-// lógica para paginar de 5 em 5 registros da tab incidents
-        .select([
+          .limit(5)
+          .offset((page -1)* 5)
+          .select([
             'incidents.*',
             'ongs.name',
             'ongs.email',
@@ -23,20 +19,24 @@ module.exports = {
             'ongs.city', 
             'ongs.uf'
         ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+        
 // selecionar quais campos retornar da tab ongs, nomear os campos
         return response.json(incidents);
     },
+
     async create(request, response) {
         const { title, description, value} = request.body;
         const ong_id = request.headers.authorization;
-
+        
         const [id] = await connection('incidents').insert({
             title,
             description,
             value,
             ong_id,
         });
-
+        
         return response.json({ id });
     },
     async delete(request, response) {
